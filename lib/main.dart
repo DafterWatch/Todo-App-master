698d706ceb5da59ee632ec47872d8a68007ae8a0
DafterWatch <80933653+DafterWatch@users.noreply.github.com>
2022-06-10 15:37:23 -0400

    v

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ String? nroboleto;
 String? origen;
 String? precio;
 String? idTicket;
+int itemCountL = 0;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -62,7 +63,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: StreamBuilder(
         // Reading Items form our Database Using the  Builder widget
-        stream: db.collection('todos').snapshots(),
+        stream: db.collection('todos').orderBy("nroboleto").snapshots(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (!snapshot.hasData) {
             return const Center(
@@ -75,25 +76,17 @@ class MyHomePage extends StatelessWidget {
               DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
               idTicket = documentSnapshot.id;
               return ListTile(
-                title: Text(documentSnapshot.id +
-                    ' ' +
-                    documentSnapshot['nroboleto'].toString() +
-                    ' / ' +
-                    documentSnapshot['cliente'] +
-                    ' / ' +
-                    documentSnapshot['fecha']),
-                subtitle: Text(documentSnapshot['origen'] +
-                    ' / ' +
-                    documentSnapshot['destino'] +
-                    ' / ' +
-                    documentSnapshot['precio'].toString() +
-                    ' Bs'),
+                title: Text(
+                    'Boleto Nro ' + documentSnapshot['nroboleto'].toString()),
+                subtitle: Text('Cliente: ' + documentSnapshot['cliente']),
                 onTap: () {
                   // Here We Will Add The Update Feature and passed the value 'true' to the is update
                   // feature.
                   showModalBottomSheet(
                     context: context,
                     builder: (BuildContext context) {
+                      itemCountL = snapshot.data?.docs.length + 1;
+                      print(itemCountL);
                       return showBottomSheetInfo(
                           context, true, documentSnapshot);
                     },
@@ -131,12 +124,60 @@ showBottomSheet(
               border: const OutlineInputBorder(),
               // Used a ternary operator to check if isUpdate is true then display
               // Update Todo.
-              labelText: isUpdate ? 'Update Todo' : 'Add Todo',
-              hintText: 'Enter An Item',
+              labelText: 'Fecha',
+              hintText: 'Ingrese la fecha DD-MM-YYYY',
             ),
             onChanged: (String _val) {
               // Storing the value of the text entered in the variable value.
-              //value = _val;
+              fecha = _val;
+            },
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              // Used a ternary operator to check if isUpdate is true then display
+              // Update Todo.
+              labelText: 'Origen',
+              hintText: 'Ingrese el lugar de origen',
+            ),
+            onChanged: (String _val) {
+              // Storing the value of the text entered in the variable value.
+              origen = _val;
+            },
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              // Used a ternary operator to check if isUpdate is true then display
+              // Update Todo.
+              labelText: 'Destino',
+              hintText: 'Ingrese el lugar de destino',
+            ),
+            onChanged: (String _val) {
+              // Storing the value of the text entered in the variable value.
+              destino = _val;
+            },
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              // Used a ternary operator to check if isUpdate is true then display
+              // Update Todo.
+              labelText: 'Precio',
+              hintText: 'Ingrese el precio',
+            ),
+            onChanged: (String _val) {
+              // Storing the value of the text entered in the variable value.
+              precio = _val;
             },
           ),
         ),
@@ -146,14 +187,14 @@ showBottomSheet(
                   MaterialStateProperty.all(Colors.lightBlueAccent),
             ),
             onPressed: () {
-              // Check to see if isUpdate is true then update the value else add the value
-              if (isUpdate) {
-                db.collection('todos').doc(documentSnapshot?.id).update({
-                  //'todo': value,
-                });
-              } else {
-                //db.collection('todos').add({'todo': value});
-              }
+              db.collection('todos').add({
+                'nroboleto': itemCountL,
+                'cliente': 'Jose',
+                'fecha': fecha,
+                'origen': origen,
+                'destino': destino,
+                'precio': int.parse(precio!)
+              });
               Navigator.pop(context);
             },
             child: isUpdate
@@ -167,144 +208,82 @@ showBottomSheet(
   );
 }
 
-showBottomSheetInfo2(
-    BuildContext context, bool isUpdate, DocumentSnapshot? documentSnapshot) {
-  // Added the isUpdate argument to check if our item has been updated
-  return Padding(
-    padding: const EdgeInsets.all(20.0),
-    /*child: Center(
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            new Text(
-              "Name :",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-            ),
-            Divider(),
-            new Text(
-              "Codebar : ",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-            ),
-            Divider(),
-            new Text(
-              "Description : ",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-            ),
-            Divider(),
-            new Text(
-              "Price : ",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-            ),
-            Divider(),
-            new Text(
-              "Stock : ",
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 8.0),
-            ),
-            Divider(),
-            Container(
-              width: 300.0,
-            ),
-          ],
-        ),
-      ),
-    ),*/
-  );
-}
-
 showBottomSheetInfo(
     BuildContext context, bool isUpdate, DocumentSnapshot? documentSnapshot) {
   // Added the isUpdate argument to check if our item has been updated
   return Padding(
     padding: const EdgeInsets.only(top: 20),
     child: FutureBuilder<DocumentSnapshot>(
-      future: db.doc('e4EMEoUAQeDVpy12x6bM').get(),
       builder: ((context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-          return Text('${data.toString()}');
-        }
-        return Text('Loading');
-      }),
-      /*builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return ListView.builder(
-          itemCount: snapshot.data?.docs.length,
-          itemBuilder: (
-            context,
-            int index,
-          ) {
-            DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
-            idTicket = documentSnapshot.id;
-            final docRef = db.collection("todos").doc("e4EMEoUAQeDVpy12x6bM");
-            docRef.get().then(
-                  (res) => {
-                    res.data()?.forEach((key, value) {
-                      switch (key.toString()) {
-                        case "fecha":
-                          fecha = value.toString();
-                          print(fecha.toString());
-                          break;
-                        case "origen":
-                          origen = value.toString();
-                          print(origen.toString());
-                          break;
-                        case "cliente":
-                          cliente = value.toString();
-                          print(cliente.toString());
-                          break;
-                        case "precio":
-                          precio = value.toString();
-                          print(precio.toString());
-                          break;
-                        case "destino":
-                          destino = value.toString();
-                          print(destino.toString());
-                          break;
-                        case "nroboleto":
-                          nroboleto = value.toString();
-                          print(nroboleto.toString());
-                          break;
-                      }
-                    })
-                  },
-                  onError: (e) => print("Error completing: $e"),
-                );
-            return ListTile(
-              title: Text(nroboleto.toString() +
-                  ' / ' +
-                  cliente.toString() +
-                  ' / ' +
-                  fecha.toString()),
-              subtitle: Text(origen.toString() +
-                  ' / ' +
-                  destino.toString() +
-                  ' / ' +
-                  precio.toString() +
-                  ' Bs'),
-            );
-          },
+        print(documentSnapshot!['nroboleto']);
+        return Center(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                // ignore: unnecessary_new
+                new Text(
+                  "Nro Boleto: " + documentSnapshot['nroboleto'].toString(),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                // ignore: prefer_const_constructors
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                ),
+                const Divider(),
+                // ignore: unnecessary_new
+                new Text(
+                  "Cliente: " + documentSnapshot['cliente'].toString(),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                // ignore: prefer_const_constructors
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                ),
+                const Divider(),
+                // ignore: unnecessary_new
+                new Text(
+                  "Fecha: " + documentSnapshot['fecha'].toString(),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                // ignore: prefer_const_constructors
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                ),
+                Divider(),
+                // ignore: unnecessary_new
+                new Text(
+                  "Precio: " + documentSnapshot['precio'].toString() + " Bs",
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                // ignore: prefer_const_constructors
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                ),
+                Divider(),
+                // ignore: unnecessary_new
+                new Text(
+                  "Origen: " + documentSnapshot['origen'].toString(),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                // ignore: prefer_const_constructors
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                ),
+                const Divider(),
+                // ignore: unnecessary_new
+                new Text(
+                  "Destino: " + documentSnapshot['destino'].toString(),
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                const Divider(),
+                Container(
+                  width: 300.0,
+                ),
+              ],
+            ),
+          ),
         );
-      },*/
+      }),
     ),
   );
 }
